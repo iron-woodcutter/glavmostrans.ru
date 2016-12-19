98ec390f3e9960fe0d918f3180eae6b7400748b5
ironWoodcutter <mike@webknote.ru>
2016-12-19 23:15:24 +0300

    animate scroll to text4

--- wp-content/themes/theme58408/assets/js/script.js
+++ wp-content/themes/theme58408/assets/js/script.js
@@ -36,7 +36,7 @@ jQuery( document ).ready(function() {
 		if (!text.hasClass('hide')) {
 			jQuery('html, body').animate({
 				scrollTop: text.offset().top - 200
-			}, 2000);
+			}, 500);
 		}
 	});
 
